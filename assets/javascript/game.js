593043879a1cc498cbb8eef5611d4d071d5a4202
Trish Mederos <trishmederos@gmail.com>
2017-11-15 18:22:32 -0700

    Final Solution

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,87 +1,113 @@
-// Random number generator.
-Math.random().toString().slice(2,11);
+$(document).ready(function(){
+// Global variables
+  var wins = 0;
+  var losses = 0;
+  var userTotal = 0;
+  var randomNumber = setRandomNumber();
+  var applePie = 0;
+  var pumpkinPie = 0;
+  var pecanPie = 0;
+  var cake = 0;
+  var appleClicks = 0;
+  var pumpkinClicks = 0;
+  var pecanClicks = 0;
+  var cakeClicks = 0;
+
+setDessertValues();
+  //
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function setRandomNumber(){
+  randomNumber = getRandomInt( 19, 120 );
+  console.log( "Random - " + randomNumber );
+   $("#randomNumber").text( randomNumber );
+   return randomNumber;
+}
+//
+function setDessertValues(){
+  applePie = getRandomInt( 1, 12 );
+  pumpkinPie = getRandomInt( 1, 12 );
+  pecanPie = getRandomInt( 1, 12 );
+  cake = getRandomInt( 1, 12 );
+  //
+ var dessertValues = [applePie, pumpkinPie, pecanPie, cake ];
+  // Next we create a for loop to create desserts for every numberOption.
+  for (var i = 0; i < dessertValues.length; i++) {
+    // Each dessert will be given a data attribute called data-value.
+    // This data attribute will be set equal to the array value.
+    $(".dessert").attr("data-value", dessertValues[i]);
+  }
+  //
+  console.log( "Apple - " + applePie);
+  console.log( "Pumpkin - " + pumpkinPie);
+  console.log( "Pecan - " + pecanPie);
+  console.log( "Cake - " + cake);
+}
+
+// Reset Game
+function resetGame(){
+  setRandomNumber();
+  setDessertValues();
+  userTotal = 0;
+  $('#userTotal').text(userTotal);
+  appleClicks = 0;
+  pumpkinClicks = 0;
+  pecanClicks = 0;
+  cakeClicks = 0;
+  $("#appleClicks").text( appleClicks );
+  $("#pumpkinClicks").text( pumpkinClicks );
+  $("#pecanClicks").text(pecanClicks);
+  $("#cakeClicks").text( cakeClicks );
+}
+//
+function playGame(){
+  if ( userTotal === randomNumber ){
+    wins++;
+    $('#wins').text(wins);
+    $("#game-msg").text("You won!");
+    resetGame();
+  }
+  else if ( userTotal > randomNumber ){
+    losses++;
+    $('#losses').text(losses);
+    $("#game-msg").text("You Lose!");
+    resetGame();
+  }
+};
+
+// On click for each image.
+$('#applePie').on('click', function(){
+  // userTotal = $("#applePie").text(applePie);
+  userTotal += applePie;
+  $("#userTotal").text(userTotal);
+  appleClicks++;
+  $("#appleClicks").text( appleClicks );
+  playGame();
+});
+$('#pumpkinPie').on('click', function(){
+  userTotal += pumpkinPie;
+  $('#userTotal').text(userTotal);
+  pumpkinClicks++;
+  $("#pumpkinClicks").text( pumpkinClicks );
+  playGame();
+});
+$('#pecanPie').on('click', function(){
+  userTotal += pecanPie;
+  $('#userTotal').text(userTotal);
+  pecanClicks++;
+  $("#pecanClicks").text(pecanClicks);
+  playGame();
+});
+
+$('#cake').on('click', function(){
+  userTotal += cake;
+  $('#userTotal').text(userTotal);
+  cakeClicks++;
+  $("#cakeClicks").text( cakeClicks );
+  playGame();
+});
+
+});
 
 
-// The Object for the user's input.
-  var user = {
-    wins: 0,
-    losses: 0,
-    guesses: 10,
-    currentLetter: "",
-    theWord: "",
-    answerArray: [],
-    // Array of Wrong Guesses
-  }
-  //
-  function pickTheWord(){
-    var word = user.wordList[Math.floor(Math.random()*user.wordList.length)];
-    user.lettersLeft = word.length;
-    return word;
-  };
-  //
-  // Make sure the user enters an alphabet character.
-  var isAlphabetCharacter = function(letter) {
-    return letter.length === 1 && /[a-z]/i.test(letter);
-  };
-
-  // Put the word into an array of letters.
-  function splitTheWordIntoLetters() {
-    user.theWordLetters = user.theWord.toUpperCase().split('');
-  };
-  // Display the stats on the page
-  function renderPage(){
-    document.getElementById("guesses-wrong").innerHTML = user.wrongGuesses;
-    document.getElementById("guesses-left").innerHTML = user.guesses;
-    document.getElementById("wins").innerHTML = user.wins;
-    document.getElementById("losses").innerHTML = user.losses;
-  };
-  // Determines if the user has guesses all of the letters by
-  // counting _'s remaining in solution array
-  var guessedAllLetters = function(guessesArr) {
-  // takes an array of letters and underscore as an argument
-    return guessesArr.indexOf("_") === -1;
-  };
-  //
-  var startNewWord = function() {
-    console.log("Secret word - " + user.theWord );
-    splitTheWordIntoLetters();
-    // Put _ in the letter places for display --- Maybe or do this on initialization
-    for (var j = 0; j < user.theWord.length; j++) {
-      user.answerArray[j] = "_";
-    }
-    document.getElementById("answerArray").innerHTML = user.answerArray.join(" ");
-  }
-  //
-  startNewWord();
-  //
-  // The Game starts whenever the user presses a key.
-  document.onkeyup = function(event) {
-  // Determines which key was pressed.
-  user.currentLetter = event.key.toUpperCase();
-  //
-  if(!isAlphabetCharacter(user.currentLetter)) {
-    return;
-  }
-  // Check for repeats
-  if(!repeatGuess(user.currentLetter)) {
-    alert("You already guessed that letter!");
-    return;
-  }
-  user.allGuesses.push( user.currentLetter );
-  //
-  if ( user.theWordLetters.indexOf( user.currentLetter ) != -1 ) {
-    for ( var x = 0; x < user.lettersLeft; x++ ){
-      if ( user.theWordLetters[x] == user.currentLetter ){
-        user.answerArray[x] = user.currentLetter;
-        user.correctGuesses++;
-        document.getElementById("answerArray").innerHTML = user.answerArray.join(" ");
-      }
-    }
-  }
-  else {
-    user.guesses--;
-    user.wrongGuesses.push( user.currentLetter );
-    renderPage();
-  }
-
-  };
